595321a24ceb30b82259b9cb9ffcfb40b280513f
Christophe Wang <christophewang@Christophes-MacBook-Pro.local>
2023-12-14 22:19:32 +0100

    test

--- src/controller/main.py
+++ src/controller/main.py
@@ -4,7 +4,6 @@ import os
 from controller.mongo import delete_all_mongo
 from controller.redis import delete_all_redis
 from controller.neo4j import insert_neo4j, delete_all_neo4j, get_all_neo4j
-from controller.elasticsearch import delete_all_elasticsearch
 
 def insert_default_values():
     insert_neo4j()
@@ -15,7 +14,6 @@ def delete_all_data():
     delete_all_mongo()
     delete_all_redis()
     delete_all_neo4j()
-    delete_all_elasticsearch()
 
     return redirect(url_for('index'))
 
