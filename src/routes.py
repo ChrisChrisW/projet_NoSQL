2ecaf32ee14f94e48490c9153737cfedb5cef5d9
Christophe Wang <christophewang@Christophes-MacBook-Pro.local>
2023-12-14 23:19:36 +0100

    delete an unset def

--- src/routes.py
+++ src/routes.py
@@ -45,39 +45,5 @@ app.route('/get_messages', methods=['GET'])(get_messages)
 app.route('/send_message', methods=['POST'])(send_message)
 app.route('/delete_message', methods=['DELETE'])(delete_message)
 
-@freezer.register_generator
-def route_generators():
-    # Routes for main
-    yield 'insert_default_values', {}
-    yield 'delete_all_data', {}
-    yield 'index', {}
-
-    # Routes for MongoDB - Spotify Playlist
-    yield 'get_all_mongo_data', {}
-    yield 'submit_mongo', {}
-    yield 'delete_mongo', {}
-
-    # Routes for Redis - Meme
-    yield 'add_memes_from_api', {}
-    yield 'get_memes', {}
-    yield 'add_meme', {}
-    yield 'edit_meme', {'meme_id': 1}  # Replace 1 with an actual meme_id
-    yield 'delete_meme', {'meme_id': 1}  # Replace 1 with an actual meme_id
-
-    # Routes for Postgres - Pokedex
-    yield 'update_pokemon_data', {}
-    yield 'get_all_pokemon', {}
-    yield 'add_pokemon', {}
-    yield 'update_pokemon', {'pokemon_id': 1}  # Replace 1 with an actual pokemon_id
-    yield 'delete_pokemon', {'pokemon_id': 1}  # Replace 1 with an actual pokemon_id
-
-    # Routes for Neo4j
-    yield 'chat', {}
-
-    # Routes for Elasticsearch - Chatbox
-    yield 'get_messages', {}
-    yield 'send_message', {}
-    yield 'delete_message', {}
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
