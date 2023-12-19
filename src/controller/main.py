4332ed04cc3de756286b796173ccff02c6e3f5bf
Christophe Wang <christophewang@Christophes-MacBook-Pro.local>
2023-12-20 00:23:43 +0100

    delete not finish app

--- src/controller/main.py
+++ src/controller/main.py
@@ -3,24 +3,19 @@ import os
 
 from controller.mongo import delete_all_mongo
 from controller.redis import delete_all_redis
-from controller.neo4j import insert_neo4j, delete_all_neo4j, get_all_neo4j
 from controller.elasticsearch import delete_all_elasticsearch
 
 def insert_default_values():
-    insert_neo4j()
-
     return redirect(url_for('index'))
 
 def delete_all_data():
     delete_all_mongo()
     delete_all_redis()
-    delete_all_neo4j()
     delete_all_elasticsearch()
 
     return redirect(url_for('index'))
 
 def index():
-    neo4j_data = get_all_neo4j()
 
-    return render_template('index.html', neo4j_data=neo4j_data,
+    return render_template('index.html',
                            spotify_clientId=os.getenv('spotify_clientId'), spotify_clientSecret=os.getenv('spotify_clientSecret'))
--- src/routes.py
+++ src/routes.py
@@ -4,7 +4,6 @@ from controller.main import *
 from controller.mongo import *
 from controller.redis import *
 from controller.postgres import *
-from controller.neo4j import *
 from controller.elasticsearch import *
 
 app = Flask(__name__)
@@ -33,20 +32,10 @@ app.route('/add_pokemon', methods=['POST'])(add_pokemon)
 app.route('/update_pokemon/<pokemon_id>', methods=['PUT'])(update_pokemon)
 app.route('/delete_pokemon/<pokemon_id>', methods=['DELETE'])(delete_pokemon)
 
-# Routes for Neo4j
-app.route('/chat', methods=['POST'])(chat)
-
-# app.route('/submit_neo4j', methods=['POST'])(submit_neo4j)
-# app.route('/edit_neo4j/<item_id>', methods=['GET', 'POST'])(edit_neo4j)
-# app.route('/delete_neo4j/<item_id>', methods=['DELETE', 'POST'])(delete_neo4j)
-
 # Routes for Elasticsearch - Chatbox
 app.route('/get_messages', methods=['GET'])(get_messages)
 app.route('/send_message', methods=['POST'])(send_message)
 app.route('/delete_message', methods=['DELETE'])(delete_message)
 
 if __name__ == '__main__':
-    app.config.update(
-        DEBUG=False,
-    )
     app.run(host='0.0.0.0', port=8080, debug=True)
