3535bd4e57b5f53f5920013931c7a0bead6c5049
Christophe Wang <christophewang@Christophes-MacBook-Pro.local>
2023-12-20 10:22:50 +0100

    only mongo and postgre

--- src/controller/main.py
+++ src/controller/main.py
@@ -2,16 +2,16 @@ from flask import render_template, redirect, url_for
 import os
 
 from controller.mongo import delete_all_mongo
-from controller.redis import delete_all_redis
-from controller.elasticsearch import delete_all_elasticsearch
+# from controller.redis import delete_all_redis
+# from controller.elasticsearch import delete_all_elasticsearch
 
 def insert_default_values():
     return redirect(url_for('index'))
 
 def delete_all_data():
     delete_all_mongo()
-    delete_all_redis()
-    delete_all_elasticsearch()
+    # delete_all_redis()
+    # delete_all_elasticsearch()
 
     return redirect(url_for('index'))
 
--- src/routes.py
+++ src/routes.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 from controller.main import *
 from controller.mongo import *
-from controller.redis import *
+# from controller.redis import *
 from controller.postgres import *
-from controller.elasticsearch import *
+# from controller.elasticsearch import *
 
 app = Flask(__name__)
 
@@ -18,24 +18,12 @@ app.route('/get_all_mongo_data', methods=['GET'])(get_all_mongo_data)
 app.route('/submit_mongo', methods=['POST'])(submit_mongo)
 app.route('/delete_mongo', methods=['DELETE'])(delete_mongo)
 
-# Routes for Redis - Meme
-app.route('/add_memes_from_api', methods=['POST'])(add_memes_from_api)
-app.route('/get_memes', methods=['GET'])(get_memes)
-app.route('/add_meme', methods=['POST'])(add_meme)
-app.route('/edit_meme/<meme_id>', methods=['PUT'])(edit_meme)
-app.route('/delete_meme/<meme_id>', methods=['DELETE'])(delete_meme)
-
 # Routes for Postgres - Pokedex
 app.route('/update_pokemon_data', methods=['POST'])(update_pokemon_data)
 app.route('/get_all_pokemon', methods=['GET'])(get_all_pokemon)
 app.route('/add_pokemon', methods=['POST'])(add_pokemon)
 app.route('/update_pokemon/<pokemon_id>', methods=['PUT'])(update_pokemon)
 app.route('/delete_pokemon/<pokemon_id>', methods=['DELETE'])(delete_pokemon)
-
-# Routes for Elasticsearch - Chatbox
-app.route('/get_messages', methods=['GET'])(get_messages)
-app.route('/send_message', methods=['POST'])(send_message)
-app.route('/delete_message', methods=['DELETE'])(delete_message)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
